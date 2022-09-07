3ceae7ce9f0d12282691d813352c99b4a891f40d
XTEP63 <110838865+XTEP63@users.noreply.github.com>
2022-09-07 09:46:16 -0500

    Inicio tarea 3

---
+++ OS22b/Tareas/tarea3.py
@@ -0,0 +1,50 @@
+from re import A
+
+
+Problema = int(input("Que problema quieres resolver 1-15: "))
+
+if Problema == 1:
+
+    #*Dos variable de tipo Float
+    #!(a) es mayor a (b) 
+
+    a = float(input("Dame un numero entero: "))
+    b = float(input("Dame otro numero entero: "))
+
+    if a > b: 
+        print("Estan en orden acendente ^w^!!!")
+    else:
+        print("No estan en orden acendente -_-")
+
+elif Problema == 2:
+    print()
+elif Problema == 3:
+    print()
+elif Problema == 4:
+    print()
+elif Problema == 5:
+    print()
+elif Problema == 6:
+    print()
+elif Problema == 7:
+    print()
+elif Problema == 8:
+    print()
+elif Problema == 9:
+    print()
+elif Problema == 10:
+    print()
+elif Problema == 11:
+    print()
+elif Problema == 12:
+    print()
+elif Problema == 13:
+    print()
+elif Problema == 14:
+    print()
+elif Problema == 15:
+    print()
+else:
+
+    print("El problema no existe XD")
+    
