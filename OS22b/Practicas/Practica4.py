1dd4b048cca083eb8516c40c9e5be52146265d8c
XTEP63 <110838865+XTEP63@users.noreply.github.com>
2022-09-19 08:22:11 -0500

    Practica 4

--- OS22b/Practicas/Practica4.py
+++ OS22b/Practicas/Practica4.py
@@ -1,3 +1,17 @@
-from re import A
+number = int(input( "Dame un número: "))
+i = 2
+toggle = 0
 
+if number%2 == 0:
+    print("No es primo")
+else:
+    while i < number**0.5:
+        if(number%i == 0):
+            toggle = 1
+            print ("No es primo ")
+            break
+        else:
+            i = i+1
+    if toggle == 0:
+        print ("Es primo")
 
