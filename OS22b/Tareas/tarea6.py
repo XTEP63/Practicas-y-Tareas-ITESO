a0540d08d4c4958b025c235600be4c29d7d24710
XTEP63 <110838865+XTEP63@users.noreply.github.com>
2022-09-29 07:27:14 -0500

    Inicio tarea 6

---
+++ OS22b/Tareas/tarea6.py
@@ -0,0 +1,59 @@
+while True:
+    while True:
+
+        try:
+            problema = int(input("Dame un numero de problema entre 1-10: "))
+        except ValueError:
+            print("Debes escribir un número -_-")
+            continue
+
+        if problema == 22:
+            exit()
+        elif problema < 1 or problema > 10:
+            print("Ese problema no existe -_-")
+            continue
+        else:
+            break
+
+    if problema == 1:
+
+        print()
+
+    elif problema == 2:
+
+        print()
+
+    elif problema == 3:
+
+        print()
+
+    elif problema == 4:
+
+        print()
+
+    elif problema == 5:
+
+        print()
+        
+    elif problema == 6:
+
+        print()
+
+    elif problema == 7: 
+
+        print()
+    elif problema == 8:
+
+        print()
+
+    elif problema == 9:
+
+        print()
+
+    elif problema == 10:
+
+        print()
+
+    
+    
+
