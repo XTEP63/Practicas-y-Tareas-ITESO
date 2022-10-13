a98f017fabefd1a30f98423a436594568f06cab5
XTEP63 <110838865+XTEP63@users.noreply.github.com>
2022-10-12 23:30:13 -0500

    public/ private

--- OS22b/Free/bin.py
+++ OS22b/Free/bin.py
@@ -1,13 +1,18 @@
 import ipaddress
 
+def IPAddress(IP:str):
+    return "Private" if ipaddress.ip_address(IP).is_private else "Public"
+
 red = input("IP: ")
 mac = input("mascara: ")
+b = input("Ip b: ")
 p = bin(int(ipaddress.IPv4Address(red)))
 m = bin(int(ipaddress.IPv4Address(mac)))
 p = p[2:]
 m = m[2:]
 print("Ip:",p)
 print("Mascara:",m)
+print(IPAddress(red))
 
 Proced_and = ""
 for i in range(len(p)):
@@ -17,4 +22,8 @@ for i in range(len(p)):
         Proced_and = Proced_and + "1"
 print("Proceso Y:",Proced_and)
 
+print("Domino brocast:",ipaddress.IPv4Network(b,strict=False).broadcast_address)
 
+#* clase A 0-127 8 bits de red 24 host
+#* clase B 128-191 16bits de red 16 host
+#* clase c 192-223 24bits de red 8 host 
