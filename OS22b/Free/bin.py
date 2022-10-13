2e48a7143c5e7c5030af48880b5a357058b941ea
XTEP63 <110838865+XTEP63@users.noreply.github.com>
2022-10-13 00:09:41 -0500

    macara

--- OS22b/Free/bin.py
+++ OS22b/Free/bin.py
@@ -1,5 +1,4 @@
 import ipaddress
-from traceback import print_tb
 
 def IPAddress(IP:str):
     return "Private" if ipaddress.ip_address(IP).is_private else "Public"
@@ -30,15 +29,24 @@ print("Domino brocast:",ipaddress.IPv4Network(b,strict=False).broadcast_address)
 
 if lista_red[0] <= 127:
     Clase = "Clase A"
-    print("ID de red:",str(lista_red[0])) 
+    I_red = str(lista_red[0]) 
+    host = str(lista_red[-1]) + "." + str(lista_red[-2]) + "." + str(lista_red[-3])
+    masca = "255.0.0.0"
 elif lista_red[0] <= 191:
     Clase = "Clase B"
-    print("ID de red:",str(lista_red[0]) + "." + str(lista_red[1])) 
+    I_red = str(lista_red[0]) + "." + str(lista_red[1])
+    host = str(lista_red[-1]) + "." + str(lista_red[-2])
+    masca = "255.255.0.0"
 elif lista_red[0] <= 223:
     Clase = "Clase C"
-    print("ID de red:",str(lista_red[0]) + "." + str(lista_red[1]) + "." + str(lista_red[2]))
+    I_red = str(lista_red[0]) + "." + str(lista_red[1]) + "." + str(lista_red[2])
+    host = str(lista_red[-1])
+    masca = "255.255.255.0"
 
 print(Clase)
+print("ID de red:",I_red)
+print("Host:", host)
+print("Mascara:", masca)
 
 #* clase A 0-127 8 bits de red 24 host
 #* clase B 128-191 16bits de red 16 host
