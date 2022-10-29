00b37904d3effd44cad42ae79223418645256f5f
XTEP63 <110838865+XTEP63@users.noreply.github.com>
2022-10-28 21:58:49 -0500

    Gis

---
+++ OS22b/Gis/Aorcado.py
@@ -0,0 +1,11 @@
+lineas = [line.rstrip('\n') for line in open("Preguntas.txt")]
+
+CARD_MAP = {
+    1: lineas[0], 
+    2: lineas[1], 
+    3: lineas[2], 
+    4: lineas[3], 
+    }
+
+#print(CARD_MAP)
+print(lineas)
