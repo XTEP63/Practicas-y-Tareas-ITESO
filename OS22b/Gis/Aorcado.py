26befca648dcceee2ff44d6f01dcafabb343da73
XTEP63 <110838865+XTEP63@users.noreply.github.com>
2022-10-28 22:14:03 -0500

    Com

--- OS22b/Gis/Aorcado.py
+++ OS22b/Gis/Aorcado.py
@@ -1,11 +1,24 @@
-lineas = [line.rstrip('\n') for line in open("G.txt")]
+from random import randint
 
-CARD_MAP = {
+
+lineas = [line.rstrip('\n') for line in open("Gis/G.txt")]
+lI = [line.rstrip('\n') for line in open("Gis/Res.txt")]
+
+
+PREGUNTAS_QUIMICA = {
     1: lineas[0], 
     2: lineas[1], 
     3: lineas[2], 
     4: lineas[3], 
     }
 
-#print(CARD_MAP)
-print(lineas)
+RESPUESTAS_QUIMICA = {
+    1: lI[0], 
+    2: lI[1], 
+    3: lI[2], 
+    4: lI[3], 
+    }
+
+print(PREGUNTAS_QUIMICA[1])
+print(RESPUESTAS_QUIMICA[1])
+#print(lineas)
